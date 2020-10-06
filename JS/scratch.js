4febb03f0110ecfa1c1c5dc46590ad079fd935ea
Nathan Rhead Cox <nrc4@hotmail.com>
2020-10-06 15:42:46 -0700

    Messed around with my code to get it to add hours with AM and PM, but failed. Moving on to today's lab.

--- JS/scratch.js
+++ JS/scratch.js
@@ -6,7 +6,7 @@ var seattle = {
   name: 'Seattle',
   minCustomersPerHour: 23, // This informaiton is provided by Pat.
   maxCustomersPerHour: 65, // This information is provided by Pat.
-  avgCookieSales: 6.3, // This informaiton is provided by Pat.
+  avgCookiesPerCustomer: 6.3, // This informaiton is provided by Pat.
   customersPerHour: [], //we use this information to generate the cookie sales per hour.
   cookieSalesPerHour: [], //This is the information we want.
 };
@@ -15,7 +15,7 @@ var tokyo = {
   name: 'Tokyo',
   minCustomersPerHour: 3, // This informaiton is provided by Pat.
   maxCustomersPerHour: 24, // This information is provided by Pat.
-  avgCookieSales: 1.2, // This informaiton is provided by Pat.
+  avgCookiesPerCustomer: 1.2, // This informaiton is provided by Pat.
   customersPerHour: [], //we use this information to generate the cookie sales per hour.
   cookieSalesPerHour: [], //This is the information we want.
 };
@@ -24,7 +24,7 @@ var dubai = {
   name: 'Dubai',
   minCustomersPerHour: 11, // This informaiton is provided by Pat.
   maxCustomersPerHour: 38, // This information is provided by Pat.
-  avgCookieSales: 3.7, // This informaiton is provided by Pat.
+  avgCookiesPerCustomer: 3.7, // This informaiton is provided by Pat.
   customersPerHour: [], //we use this information to generate the cookie sales per hour.
   cookieSalesPerHour: [], //This is the information we want.
 };
@@ -33,7 +33,7 @@ var paris = {
   name: 'Paris',
   minCustomersPerHour: 20, // This informaiton is provided by Pat.
   maxCustomersPerHour: 38, // This information is provided by Pat.
-  avgCookieSales: 2.3, // This informaiton is provided by Pat.
+  avgCookiesPerCustomer: 2.3, // This informaiton is provided by Pat.
   customersPerHour: [], //we use this information to generate the cookie sales per hour.
   cookieSalesPerHour: [], //This is the information we want.
 };
@@ -42,7 +42,7 @@ var lima = {
   name: 'Lima',
   minCustomersPerHour: 2, // This informaiton is provided by Pat.
   maxCustomersPerHour: 16, // This information is provided by Pat.
-  avgCookieSales: 4.6, // This informaiton is provided by Pat.
+  avgCookiesPerCustomer: 4.6, // This informaiton is provided by Pat.
   customersPerHour: [], //we use this information to generate the cookie sales per hour.
   cookieSalesPerHour: [], //This is the information we want.
 };
@@ -57,7 +57,7 @@ function loopCalculator(object) {
   for (var i = 0; i < 14; i++) {
     var customersPerHour = randomCustomersPerHour(object.minCustomersPerHour, object.maxCustomersPerHour);
     object.customersPerHour.push(customersPerHour); // Pushes a new random number from the randCustomersPerHour function to the customersPerHour array of each object.
-    object.cookieSalesPerHour.push(Math.floor(object.avgCookieSales * customersPerHour)); // This pushes the product of average cookie sales and number of customers per hour to the cookieSalesPerHour array of each object.
+    object.cookieSalesPerHour.push(Math.floor(object.avgCookiesPerCustomer * customersPerHour)); // This pushes the product of average cookie sales and number of customers per hour to the cookieSalesPerHour array of each object.
     // console.log(`Customers per hour: ${object.customersPerHour[i]}.`);
     // console.log(`Cookies per hour: ${object.cookieSalesPerHour[i]}.`);
   }
@@ -72,33 +72,54 @@ loopCalculator(lima);
 //consider creatting an array for the city names.
 //nested loop: outer loop through each city; inner through cookie sales for each location
 
-var cityArray = [seattle.name, tokyo.name, dubai.name, paris.name, lima.name];
-var cookieSales = [seattle.cookieSalesPerHour, tokyo.cookieSalesPerHour, dubai.cookieSalesPerHour, paris.cookieSalesPerHour, lima.cookieSalesPerHour];
+var cityArray = [seattle, tokyo, dubai, paris, lima];
+var cookieSales = [seattle, tokyo, dubai, paris, lima];
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+for (var i = 0; i < cityArray.length; i++) {
+  var cityName = cityArray[i].name;
+  // console.log(cityName);
+
+  // This is the parent element
+  var parentElement = document.getElementById(cityName);
+
+  // This is a child element for city names
+  var thElement = document.createElement('th');
+  thElement.textContent = cityName;
+  parentElement.appendChild(thElement);
+  //   console.log(thElement, cityHeader);
+
+  var runningCount = 0;
 
   for (var j = 0; j < 14; j++) {
-    var hour = j + 1;
-    var hourlyCookieSales = cookieSales[j];
+    var hourlyCookieSales = cookieSales[i].cookieSalesPerHour;
+    // console.log(hourlyCookieSales);
+
+    var cookieSalesPh = document.getElementById(cityArray[i].name);
+    var tdElement = document.createElement('td');
+    tdElement.textContent = `${hours}: ${hourlyCookieSales[j]}`;
+    // cookieSalesPh.appendChild(tdElement);
+
+    runningCount += + hourlyCookieSales[j];
+
   }
-
-
-function city(object) {
-    for (var i = 0; i < cityArray.length; i++) {
-        var cityName = cityArray[i];
-      
-  
-  var cityHeader = document.getElementById(object.name);
-  var thElement = document.createElement('th');
-  thElement.textContent = cityName[i];
-  cityHeader.appendChild(thElement);
-}
+  var tdTotalElement = document.createElement('td');
+  tdTotalElement.textContent = `Daily total = ${runningCount}`;
+  // cookieSalesPh.appendChild(tdTotalElement);
 }
 
 
-city(seattle);
+// function sumOfDailyCookieSales(dynamicArray) { 
+//     var sum = 1;
+//       for (var i = 0; i < dynamicArray.length; i++) {
+//           totalSum = sum(totalProduct, dynamicArray[i])[0]; // 1: 1 * 1 = 1; 2: 1 * 2 = 2; 3: 2 * 3 = 6; 4: 6 * 4 = 24; 5: 24 * 5 = 120;
+//       }
+//     }  
+
+
 
 
 // var tokyoCityHeader = document.getElementById('tokyo');
 // var headerElement2 = document.createElement('header');
 // headerElement2.textContent = tokyo.name;
-// tokyoCityHeader.appendChild(headerElement2);
+// tokyoCityHeader.appendChild(headerElement2)
