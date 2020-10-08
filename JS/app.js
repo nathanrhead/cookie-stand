08a524ac1b1f0e9fc0d9edf5bfb37ebc21c4afba
Nathan Rhead Cox <nrc4@hotmail.com>
2020-10-07 17:11:13 -0700

    Finished getting the hourly totals, with Simon's and Skyler's help.

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,14 @@
 'use strict';
+
+
+
 var hours = ['Store Locations', '6:00 a.m.', '7:00 a.m.', '8:00 a.m.', '9:00 a.m.', '10:00 a.m.', '11:00 a.m.', '12:00 p.m.', '1:00 p.m.', '2:00 p.m.', '3:00 p.m.', '4:00 p.m.', '5:00 p.m.', '6:00 p.m.', '7:00 p.m.', 'Grand Total'];
+
+var storeLocations = [];
+console.log(storeLocations);
+
+// var cookiesSoldPerHourGlobalAray = [];
+// console.log(cookiesSoldPerHourGlobalAray);
 
 var parentElementThead = document.getElementById('hours');
 // The parent element for the header (each hour)
@@ -7,18 +16,21 @@ var parentElementThead = document.getElementById('hours');
 var parentElementTbody = document.getElementById('cityRow');
 // The parent element for the rows of data (including the city names
 
-var parentElementTfoot = document.getElementById('');
+var parentElementTfoot = document.getElementById('hourlyTotals');
 // The parent element for the footer (totals for all stores per hour)
 
 
-function City(name, minimumCustomersPerHour, maximumCustomersPerHour, averageCookiesPerCustomer, totalCookiesForTheDay) {
+function City(name, minimumCustomersPerHour, maximumCustomersPerHour, averageCookiesPerCustomer) {
   this.name = name;
   this.minimumCustomersPerHour = minimumCustomersPerHour;
   this.maximumCustomersPerHour = maximumCustomersPerHour;
   this.averageCookiesPerCustomer = averageCookiesPerCustomer;
-  this.totalCookiesForTheDay = totalCookiesForTheDay;
+  this.totalCookiesForTheDay = 0;
   this.customersPerHour = [];
   this.cookiesSoldPerHour = [];
+
+  storeLocations.push(this);
+  // cookiesSoldPerHourGlobalAray.push(this.cookiesSoldPerHour);
 }
 
 City.prototype.generateCustomersPerHour = function(){
@@ -36,8 +48,8 @@ City.prototype.cookiesSoldEachHour = function() {
   for(var i=0; i<hours.length - 2; i++){
     var cookiesSoldThisHour = Math.ceil(this.customersPerHour[i] * this.averageCookiesPerCustomer);
     this.totalCookiesForTheDay += cookiesSoldThisHour;
+
     this.cookiesSoldPerHour.push(cookiesSoldThisHour);
-    console.log(this.cookiesSoldPerHour);
   }
 };
 
@@ -52,16 +64,19 @@ City.prototype.renderRows = function() {
   for (var j = 0; j < hours.length - 2; j++) {
     var tdChildData = document.createElement('td');
     tdChildData.textContent = this.cookiesSoldPerHour[j];
-    console.log(this.cookiesSoldPerHour);
     trChild.appendChild(tdChildData);
+
+    // var tdChildCityTotal = document.createElement('td');
+    // tdChildCityTotal.textContent = dailyTotalPerCity[j];
+    // trChild.appendChild(tdChildCityTotal);
   }
 };
 
-var seattle = new City('Seattle', 23, 65, 6.3, 0);
-var tokyo = new City('Tokyo', 3, 24, 1.2, 0);
-var dubai = new City('Dubai', 11, 38, 3.7, 0);
-var paris = new City('Paris', 20, 38, 2.3, 0);
-var lima = new City('Lima', 2, 16, 4.6, 0);
+new City('Seattle', 23, 65, 6.3);
+new City('Tokyo', 3, 24, 1.2);
+new City('Dubai', 11, 38, 3.7);
+new City('Paris', 20, 38, 2.3);
+new City('Lima', 2, 16, 4.6);
 
 function renderHeaderHours() {
   var trChild = document.createElement('tr');
@@ -76,48 +91,24 @@ function renderHeaderHours() {
 function renderFooterTotals() {
   var trChild = document.createElement('tr');
   var tdChildFoot = document.createElement('td');
-  tdChildFoot.textContent = this.totalCookiesForTheDay
-};
+  tdChildFoot.textContent = 'Hourly Totals';
+  trChild.appendChild(tdChildFoot);
+  parentElementTfoot.appendChild(trChild);
+
+  for (var i = 0; i < hours.length - 2; i++) {
+    var dailyCookieSalesAllLocations = 0;
+
+    for (var j = 0; j < storeLocations.length; j++) {
+      dailyCookieSalesAllLocations += storeLocations[j].cookiesSoldPerHour[i];
+      var tdFooterTotal = document.createElement('td');
+      tdFooterTotal.textContent = dailyCookieSalesAllLocations;
+    }
+    trChild.appendChild(tdFooterTotal);
+  }
+}
 
 renderHeaderHours();
-
-seattle.renderRows();
-tokyo.renderRows();
-dubai.renderRows();
-paris.renderRows();
-lima.renderRows();
-
-
-
-
-
-
-// console.log(seattle, tokyo, dubai, paris, lima);
-
-
-
-
-// render: function(){
-//   // get the section with the id of seattle from the DOM - that is going to be our parent element for the h2
-//   var parentSection = document.getElementById('seattle');
-
-//   // create an h2
-//   var headingSeattle = document.createElement('h2');
-//   // fill it with content - this.name
-//   headingSeattle.textContent = this.name;
-//   // append it to the parent
-//   parentSection.appendChild(headingSeattle);
-
-//   // get the ul with the id of seattle-sales from the DOM - that is going to be our parent element
-//   var salesList = document.getElementById('seattle-sales');
-
-//   // loop over our cookieSoldEachHour array
-//   for(var i=0; i<this.cookiesSoldEachHour.length; i++){
-//     // create an li
-//     var liElement = document.createElement('li');
-//     // fill it with the content from the cookies sold each hour
-//     liElement.textContent = `${hours[i]}: ${this.cookiesSoldEachHour[i]} cookies`;
-//     // append it to the parent
-//     salesList.appendChild(liElement);
-//   }
-
+for (var i = 0; i < storeLocations.length; i++) {
+  storeLocations[i].renderRows();
+}
+renderFooterTotals();
